dfc870adb602f1727b873d88f9c88401cb1f3bab
Mostafa Roshdy <66712535+mostafaroshdy1@users.noreply.github.com>
2024-03-24 10:26:02 +0200

    Initial commit

---
+++ app/Http/Controllers/FollowerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Follower;
+use Illuminate\Http\Request;
+
+class FollowerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Follower $follower)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Follower $follower)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Follower $follower)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Follower $follower)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+
+// posts
+Route::get('/posts');
+Route::get('/posts/create');
+Route::get('/posts/{id}');
+Route::get('/posts/{id}/edit');
+Route::post('/posts');
+Route::put('/posts/{id}');
+Route::delete('/posts/{id}');
+
+// comments
+Route::get('/posts/{id}/comments'); // show all comments for specific post
+Route::post('/posts/{id}/comments'); // adds comment to post
+Route::delete('/posts/{id}/comments/{id}'); // delete a comment
+
+// comments reactions
+Route::post('/posts/{id}/comments/{id}/likes'); // modify the number of likes to a specific comment (one to many relationship with users)
+Route::delete('/posts/{id}/comments/{id}/likes'); // modify the number of likes to a specific comment (one to many relationship with users)
+
+
+// users
+Route::post('/users/{id}/followers'); // create followers table needed
+Route::delete('/users/{id}/followers');
+
+Route::fallback(function () {
+    return "Route not found";
+});
